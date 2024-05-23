7b11172053614ce832a71703d5e3ba5903dae074
Aaron Geller <aaron.s.geller@gmail.com>
2024-05-23 07:44:47 -0600

    Use get_bs_srate.m.

--- do_UCHSN_movbiga.m
+++ do_UCHSN_movbiga.m
@@ -1,4 +1,8 @@
-function do_UCHSN_movbiga(offset_s, duration_s, absolute_offset)
+function do_UCHSN_movbiga(offset_s, duration_s, absolute_offset, outputdir)
+
+if ~exist('outputdir', 'var')
+    outputdir = 'granger';
+end
 
 % offset_s: file-specific offset
 
@@ -8,20 +12,22 @@ function do_UCHSN_movbiga(offset_s, duration_s, absolute_offset)
 
 % Fxy and Fyx have dimensions: channels x freq x time
 
-whole_file = 'data_block001';
+whole_file = 'data_block001_notch.mat';
 onset_file = 'data_block001_02_notch.mat';
 prefix = '/Users/aaron/Documents/brainstorm_db/IEEG_visualization/data/UCHSN/UCHSN_09_04_23__16_36_19';
 
 load(fullfile(prefix, 'channel.mat'));
-load(fullfile(prefix, onset_file));
+% load(fullfile(prefix, onset_file));
+load(fullfile(prefix, whole_file));
 
-srate = 200;
+srate = get_bs_srate(History); % History is saved in brainstorm datafile
 
 start_sample = round(srate*offset_s) + 1;
 end_sample = start_sample + round(srate*duration_s);
 
 [Fxy, Fyx] = do_seeded_gc(F, srate, Channel, 'LCM2', start_sample, ...
-                          end_sample, absolute_offset, 'UCHSN/figs/granger_onset');
+                          end_sample, absolute_offset, ...
+                          fullfile('UCHSN/figs', outputdir));
 
 % save Fxy matrices
 output_str = ['UCHSN_' num2str(absolute_offset) '_' num2str(duration_s)];
