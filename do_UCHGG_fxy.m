07e3732de968e7f1f176b67d5208b94d18dc5ad0
Aaron Geller <aaron.s.geller@gmail.com>
2024-05-23 07:45:44 -0600

    Use absolute_offset field.

--- do_UCHGG_fxy.m
+++ do_UCHGG_fxy.m
@@ -1,13 +1,13 @@
-function do_UCHGG_fxy(postfix)
+function do_UCHGG_fxy(postfix, absolute_offset)
 
 prefix = '/Users/aaron/Documents/brainstorm_db/IEEG_visualization/data/UCHGG/UCHGG_25_08_23__21_20_17';
 load(fullfile(prefix, 'channel.mat'));
 [~, channel_names, ~] = exclude_channels([], {Channel.Name}, 'RPI1'); 
 
-forwardmatname = ['Fxy' postfix];
-backwardmatname = ['Fyx' postfix];
+forwardmatname = ['Fxy_' postfix];
+backwardmatname = ['Fyx_' postfix];
 forwardstruct = load(forwardmatname);
 backwardstruct = load(backwardmatname);
 
 do_fxy_plots(eval(['forwardstruct.' forwardmatname]), eval(['backwardstruct.' backwardmatname]), ...
-             channel_names, 'RPI1', 5:5:100, 60, 200, 'UCHGG/figs/granger'); 
+             channel_names, 'RPI1', 5:5:100, 151, 200, 'UCHGG/figs/granger'); 
