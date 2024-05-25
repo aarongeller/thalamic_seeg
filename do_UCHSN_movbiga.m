dd788e40f733f8966b78e07c7865424a5f940dd4
Aaron Geller <aaron.s.geller@gmail.com>
2024-05-25 05:54:05 -0600

    Calls do_fxy_plots.

--- do_UCHSN_movbiga.m
+++ do_UCHSN_movbiga.m
@@ -17,17 +17,19 @@ onset_file = 'data_block001_02_notch.mat';
 prefix = '/Users/aaron/Documents/brainstorm_db/IEEG_visualization/data/UCHSN/UCHSN_09_04_23__16_36_19';
 
 load(fullfile(prefix, 'channel.mat'));
-% load(fullfile(prefix, onset_file));
-load(fullfile(prefix, whole_file));
+load(fullfile(prefix, onset_file));
+% load(fullfile(prefix, whole_file));
 
 srate = get_bs_srate(History); % History is saved in brainstorm datafile
 
 start_sample = round(srate*offset_s) + 1;
 end_sample = start_sample + round(srate*duration_s);
 
-[Fxy, Fyx] = do_seeded_gc(F, srate, Channel, 'LCM2', start_sample, ...
-                          end_sample, absolute_offset, ...
-                          fullfile('UCHSN/figs', outputdir));
+gc_info = do_seeded_gc(F, srate, Channel, 'LCM2', start_sample, ...
+                       end_sample);
+
+do_fxy_plots(Fxy, Fyx, channel_names, seedstr, freqs, ...
+             startsample/srate + absolute_offset, srate, figsdir);
 
 % save Fxy matrices
 output_str = ['UCHSN_' num2str(absolute_offset) '_' num2str(duration_s)];
