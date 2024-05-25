ad6d2397b0b24415b9cd4dfdf55ab5f72720bd84
Aaron Geller <aaron.s.geller@gmail.com>
2024-05-24 20:07:25 -0600

    Added EKG.

--- exclude_channels.m
+++ exclude_channels.m
@@ -1,6 +1,6 @@
 function [m, channel_names, seednum] = exclude_channels(dat, channel_names, seedstr)
 skip_channels = {'SpO2', 'EtCO2', 'Pulse', 'CO2Wave', 'XXX1', 'XXX2', ...
-                 'EKG1', 'C451', 'C461', 'Annotations'};
+                 'EKG', 'EKG1', 'C451', 'C461', 'Annotations'};
 seednum = find(strcmpi(seedstr, channel_names));
 indexvec = zeros(1, length(channel_names));
 for i=1:length(skip_channels)
