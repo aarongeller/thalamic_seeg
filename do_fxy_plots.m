fd987be1d99b6b2c25346aab4434faea32018246
Aaron Geller <aaron.s.geller@gmail.com>
2024-06-05 19:46:22 -0600

    Plots dashed lines for seizure onset and offset.

--- do_fxy_plots.m
+++ do_fxy_plots.m
@@ -1,4 +1,5 @@
-function do_fxy_plots(gc_info, offset, figsdir, tfsclim, zclim)
+function do_fxy_plots(gc_info, offset, figsdir, tfsclim, zclim, ...
+                      sz_onset_s, sz_offset_s)
 
 if ~exist(figsdir, 'dir')
     mkdir(figsdir);
@@ -12,6 +13,14 @@ end
 backwarddir = fullfile(figsdir, 'backward');
 if ~exist(backwarddir, 'dir')
     mkdir(backwarddir);
+end
+
+if ~exist('sz_onset_s', 'var')
+    sz_onset_s = nan;
+end
+
+if ~exist('sz_offset_s', 'var')
+    sz_offset_s = nan;
 end
 
 tic;
@@ -32,13 +41,14 @@ parfor i=1:size(gc_info.Fxy,1)
     figname = [sprintf('%03d', i) '_' gc_info.seedstr '_' channel_names{i} '.png'];
     figpath = fullfile(forwarddir, figname);
     titstr = [gc_info.seedstr ' -> ' channel_names{i}];
-    do_tfs_fig(squeeze(gc_info.Fxy(inds(i),:,:)), tfsclim, gc_info.freqs, gc_info.srate, offset, titstr, figpath);
+    do_tfs_fig(squeeze(gc_info.Fxy(inds(i),:,:)), tfsclim, gc_info.freqs, ...
+               gc_info.srate, offset, titstr, figpath, sz_onset_s, sz_offset_s);
 
     zfigname = ['z_' sprintf('%03d', i) '_' gc_info.seedstr '_' channel_names{i} '.png'];
     zfigpath = fullfile(forwarddir, zfigname);
     ztitstr = ['Z-Score ' gc_info.seedstr ' -> ' channel_names{i}];
     do_tfs_fig(squeeze(zFxy(inds(i),:,:)), zclim, gc_info.freqs, gc_info.srate, offset, ...
-               ztitstr, zfigpath);
+               ztitstr, zfigpath, sz_onset_s, sz_offset_s);
 
     pause(100/total);
     ppm.increment();
@@ -52,13 +62,14 @@ parfor i=1:size(gc_info.Fyx,1)
     figname = [sprintf('%03d', i) '_' channel_names{i} '_' gc_info.seedstr '.png'];
     figpath = fullfile(backwarddir, figname);
     titstr = [channel_names{i} ' -> ' gc_info.seedstr];
-    do_tfs_fig(squeeze(gc_info.Fyx(inds(i),:,:)), tfsclim, gc_info.freqs, gc_info.srate, offset, titstr, figpath);
+    do_tfs_fig(squeeze(gc_info.Fyx(inds(i),:,:)), tfsclim, gc_info.freqs, ...
+               gc_info.srate, offset, titstr, figpath, sz_onset_s, sz_offset_s);
 
     zfigname = ['z_' sprintf('%03d', i) '_' channel_names{i} '_' gc_info.seedstr '.png'];
     zfigpath = fullfile(backwarddir, zfigname);
     ztitstr = ['Z-Score ' channel_names{i} ' -> ' gc_info.seedstr];
     do_tfs_fig(squeeze(zFyx(inds(i),:,:)), zclim, gc_info.freqs, gc_info.srate, offset, ...
-               ztitstr, zfigpath);
+               ztitstr, zfigpath, sz_onset_s, sz_offset_s);
 
     pause(100/total);
     ppm.increment();
@@ -68,12 +79,32 @@ delete(ppm);
 close all;
 toc;
 
-function do_tfs_fig(dat, cl, freqs, srate, offset, titstr, figpath)
+function do_tfs_fig(dat, cl, freqs, srate, offset, titstr, figpath, ...
+                    sz_onset_s, sz_offset_s)
 figure('visible', 'off'); 
 imagesc(dat); 
 axis xy;
 colorbar; 
 clim(cl);
+num_samples_x = size(dat,2);
+num_samples_y = size(dat,1);
+dur_s = num_samples_x/srate;
+sz_onset_x = num_samples_x*(sz_onset_s - offset)/dur_s;
+sz_offset_x = num_samples_x*(sz_offset_s - offset)/dur_s;
+lwid = 4;
+if ~isnan(sz_onset_s)
+    hold on;
+    plot([sz_onset_x sz_onset_x], [1 num_samples_y], 'g--', 'linewidth', ...
+         lwid);
+    hold off;
+end
+if ~isnan(sz_offset_s)
+    hold on;
+    plot([sz_offset_x sz_offset_x], [1 num_samples_y], 'r--', 'linewidth', ...
+         lwid);
+    hold off;
+end
+
 % if ~exist('isz', 'var') || isz==0
 %     clim(cl);
 % end
