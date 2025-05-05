ba39aa133218af34cc462db2a411fa503b7b0ef3
Aaron Geller <aaron.s.geller@gmail.com>
2025-05-04 19:43:23 -0600

    Added thresholded TFS figs.

--- make_gc_pdf.py
+++ make_gc_pdf.py
@@ -8,7 +8,9 @@ topdir = sys.argv[2]
 
 figspath = os.path.join("analyses", subjname, "figs", topdir)
 figspath_forward = os.path.join(figspath, "forward")
+figspath_forwardthresh = os.path.join(figspath, "forward_thresh")
 figspath_backward = os.path.join(figspath, "backward")
+figspath_backwardthresh = os.path.join(figspath, "backward_thresh")
 figspath_diff = os.path.join(figspath, "diff")
 
 texfname = os.path.join("analyses", subjname, subjname + "_" + topdir + ".tex")
@@ -48,14 +50,21 @@ imgfiles_backward.sort()
 imgfiles_diff = glob(os.path.join(figspath_diff, '*.png'))
 imgfiles_diff.sort()
 
+imgfiles_thresh_forward = glob(os.path.join(figspath_forwardthresh, '*.png'))
+imgfiles_thresh_forward.sort()
+imgfiles_thresh_backward = glob(os.path.join(figspath_backwardthresh, '*.png'))
+imgfiles_thresh_backward.sort()
+
 imgsize = 0.37
-total_pairs = int(len(imgfiles_forward)/2)
+clusterpics = 6
+#total_pairs = int((len(imgfiles_forward)-clusterpics)/2)
+total_pairs = int((len(imgfiles_forward))/2)
 
 imgsize2 = 0.5
 
 for i,f in enumerate(imgfiles_forward):
     fparts = os.path.basename(f).split('_')
-    if fparts[0]=="z":
+    if fparts[0]=="z" or fparts[0]=='thresh':
         break
     elif fparts[0]=="400":
         texf.write("\pagebreak\n")
@@ -73,7 +82,7 @@ for i,f in enumerate(imgfiles_forward):
         + str(imgsize) + "\\textwidth]{" + imgfiles_backward[i+total_pairs] + "}\n\end{tabular}\n\\\\\n"
     texf.write(thisline)
 
-postamble = "\\end{longtable}\n\\end{landscape}\n\n"
+postamble = "\\end{longtable}\n"
 texf.write(postamble)
 
 thisline = "\pagebreak\n\\begin{tabular}{cc}\\\\\n\includegraphics[width=" + str(imgsize2) \
@@ -82,7 +91,55 @@ thisline = "\pagebreak\n\\begin{tabular}{cc}\\\\\n\includegraphics[width=" + str
     + "\\textwidth]{" + imgfiles_diff[1] + "}\\\\\n\\end{tabular}\n\n"
 texf.write(thisline)
 
-texf.write("\\end{document}\n")
+# forward threshold plots
+thisline = "\pagebreak\n\hspace{-4.5cm}\\begin{tabular}{|c|c|c|c|c|}\n\hline\n & Raw Z-Score & Thresholded, & Thresholded, & Thresholded," \
+    + "\\\\\n& & non-corrected & pixel-corrected & cluster-corrected\\\\\hline\n" \
+    + "Forward IOZ & " \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_forward[-2] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_forward[0] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_forward[1] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_forward[2] + "}\n\end{tabular}\\\\\hline\n" \
+    + "Forward non-IOZ & " \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_forward[-1] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_forward[3] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_forward[4] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_forward[5] + "}\n\end{tabular}\\\\\hline\n" \
+    + "\\end{tabular}\n\n"
+texf.write(thisline)
+
+# backward threshold plots
+thisline = "\pagebreak\n\hspace{-4.5cm}\\begin{tabular}{|c|c|c|c|c|}\n\hline\n & Raw Z-Score & Thresholded, & Thresholded, & Thresholded," \
+    + "\\\\\n& & non-corrected & pixel-corrected & cluster-corrected\\\\\hline\n" \
+    + "Backward IOZ & " \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_backward[-2] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_backward[0] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_backward[1] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_backward[2] + "}\n\end{tabular}\\\\\hline\n" \
+    + "Backward non-IOZ & " \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_backward[-1] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_backward[3] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_backward[4] + "}\n\end{tabular}\n&\n" \
+    + "\\begin{tabular}{c}\n\\includegraphics[width=" \
+    + str(imgsize) + "\\textwidth]{" + imgfiles_thresh_backward[5] + "}\n\end{tabular}\\\\\hline\n" \
+    + "\\end{tabular}\n\n"
+texf.write(thisline)
+
+texf.write("\\end{landscape}\n\n\\end{document}\n")
 texf.close()
 
 os.system("pdflatex -output-directory " + os.path.join("analyses", subjname) + " " + texfname)
